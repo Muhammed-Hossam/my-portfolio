c10061991d0b3d632105e16ddcbf9ae9d706660f
Mohamed Haroun <developer.muhamed@gmail.com>
2024-06-13 07:17:40 +0300

    update my cv

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="Home mb-28">
-    <div class="flex flex-col xl:flex-row-reverse xl:justify-between justify-center items-center gap-40">
-      <PictureOrbit />
-      <div class="grid xl:grid-rows-3 xl:auto-rows-fr  mt-6 xl:mt-0 w-full xl:w-auto xl:h-[25rem]">
-        <h1 class="text-2xl lg:text-4xl xl:text-3xl text-center xl:text-start font-medium tracking-wider text-blue-700 xl:inline mt-auto p-2 md:p-0">
-          Hi, I'm Mohamed Haroun
-          <img src="../assets/icons/waving.png" class="inline" width="38" height="38" alt="waving-icon">
-        </h1>
-        <TypewriterEffect />
-        <DownloadCVButton />
+    <div class="container">
+      <div class="flex flex-col xl:flex-row-reverse xl:justify-between justify-center items-center gap-40">
+        <PictureOrbit />
+        <div class="grid xl:grid-rows-3 xl:auto-rows-fr  mt-6 xl:mt-0 w-full xl:w-auto xl:h-[25rem]">
+          <h1 class="text-2xl lg:text-4xl xl:text-3xl text-center xl:text-start font-medium tracking-wider text-blue-700 xl:inline mt-auto p-2 md:p-0">
+            Hi, I'm Mohamed Haroun
+            <img src="../assets/icons/waving.png" class="inline" width="38" height="38" alt="waving-icon">
+          </h1>
+          <TypewriterEffect />
+          <DownloadCVButton />
+        </div>
       </div>
     </div>
   </div>
@@ -19,8 +21,6 @@
 import DownloadCVButton from '../components/DownloadCVButton.vue';
 import PictureOrbit from '../components/PictureOrbit.vue';
 import TypewriterEffect from '../components/TypewriterEffect.vue';
-
-
 </script>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,41 @@ export default {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '540px',
+        md: '720px',
+        lg: '960px',
+        xl: '1140px',
+        '2xl': '1320px', // Add this to expand for larger screens
+      }
+    },
     extend: {
+      screens: {
+        xs: '480px',
+        sm: '576px',  // Bootstrap sm breakpoint
+        md: '768px',  // Bootstrap md breakpoint
+        lg: '992px',  // Bootstrap lg breakpoint
+        xl: '1200px', // Bootstrap xl breakpoint
+        '2xl': '1400px', // Bootstrap xxl breakpoint
+      },
+      gridTemplateColumns: {
+        // Bootstrap-like grid system
+        '1': 'repeat(1, minmax(0, 1fr))',
+        '2': 'repeat(2, minmax(0, 1fr))',
+        '3': 'repeat(3, minmax(0, 1fr))',
+        '4': 'repeat(4, minmax(0, 1fr))',
+        '5': 'repeat(5, minmax(0, 1fr))',
+        '6': 'repeat(6, minmax(0, 1fr))',
+        '7': 'repeat(7, minmax(0, 1fr))',
+        '8': 'repeat(8, minmax(0, 1fr))',
+        '9': 'repeat(9, minmax(0, 1fr))',
+        '10': 'repeat(10, minmax(0, 1fr))',
+        '11': 'repeat(11, minmax(0, 1fr))',
+        '12': 'repeat(12, minmax(0, 1fr))',
+      },
       backgroundColor: {
         light: "#EEF1FF",
         dark: "#061532"
